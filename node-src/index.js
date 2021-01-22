aebee5274d2b1c2f77b959d15a05e7a0a36be808
Damon Perron-Laurin <damonperronlaurin@gmail.com>
2021-01-22 18:36:58 -0500

    Dockerise node

--- node-src/index.js
+++ node-src/index.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const Twig = require('twig');
 const app = express();
-const port = 8081;
-
-app.get('/', (req, res) => {
-  Twig.renderFile('../templates/index.twig', { name: 'lele' }, (err, html) => {
-    res.send(html);
-  });
-});
+const port = 9000;
 
 app.listen(port, () => {
   console.log(`App listening on port: ${port}`);
+});
+
+app.get('/', (req, res) => {
+  console.log('HELLO DAMNIT');
+  res.send('Hello papi');
 });
